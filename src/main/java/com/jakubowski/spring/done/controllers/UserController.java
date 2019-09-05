2334e61bc1e788330e9fc567d56b66bc382bf7f5
Artur Jakubowski <jakubowski1005@gmail.com>
2019-09-05 19:35:16 +0200

    Clean the mess with user controller.

--- src/main/java/com/jakubowski/spring/done/controllers/UserController.java
+++ src/main/java/com/jakubowski/spring/done/controllers/UserController.java
@@ -3,14 +3,8 @@ package com.jakubowski.spring.done.controllers;
 import com.jakubowski.spring.done.entities.User;
 import com.jakubowski.spring.done.entities.UserProperties;
 import com.jakubowski.spring.done.entities.UserStatistics;
-import com.jakubowski.spring.done.repositories.UserPropertiesRepository;
-import com.jakubowski.spring.done.repositories.UserRepository;
-import com.jakubowski.spring.done.repositories.UserStatisticsRepository;
-import com.jakubowski.spring.done.security.JwtProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.jakubowski.spring.done.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -19,107 +13,46 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class UserController {
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    private JwtProvider jwtProvider;
+    private UserService userService;
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private UserPropertiesRepository userPropertiesRepository;
-
-    @Autowired
-    private UserStatisticsRepository userStatisticsRepository;
 
     @GetMapping("/users/{userId}")
-    public User getUserById(@PathVariable long userId, @RequestHeader(value = "Authorization") String authorizationHeader) {
-
-        if(!jwtProvider.isUserAuthorized(userId, authorizationHeader)) return null;
-
-        return userRepository.findById(userId).get();
+    public User getUserById(@PathVariable long userId,
+                            @RequestHeader(value = "Authorization") String authorizationHeader) {
+        return userService.getUserById(userId, authorizationHeader);
     }
 
     @GetMapping("/users/{userId}/properties")
     public UserProperties getUserProperties(@PathVariable long userId,
                                             @RequestHeader(value = "Authorization") String authorizationHeader) {
-
-        if(!jwtProvider.isUserAuthorized(userId, authorizationHeader)) {
-           return null;
-        }
-
-        return userRepository.findById(userId).get().getUserProperties();
+        return userService.getUserProperties(userId, authorizationHeader);
     }
 
     @GetMapping("/users/{userId}/statistics")
     public UserStatistics getUserStatistics(@PathVariable long userId,
                                             @RequestHeader(value = "Authorization") String authorizationHeader) {
-
-        if(!jwtProvider.isUserAuthorized(userId, authorizationHeader)) {
-            return null;
-        }
-
-        return userRepository.findById(userId).get().getUserStatistics();
+        return userService.getUserStatistics(userId, authorizationHeader);
     }
 
     @DeleteMapping("/users/{userId}")
     public ResponseEntity<?> deleteUser(@PathVariable long userId,
                                         @RequestHeader(value = "Authorization") String authorizationHeader) {
-
-        if(!jwtProvider.isUserAuthorized(userId, authorizationHeader)) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
-
-        userRepository.deleteById(userId);
-        return ResponseEntity.noContent().build();
+        return userService.deleteUser(userId, authorizationHeader);
     }
 
     @PutMapping("/users/{userId}/properties")
     @Transactional
     public ResponseEntity<?> updateUserProperties(@PathVariable long userId, @RequestBody UserProperties userProperties,
                                                   @RequestHeader(value = "Authorization") String authorizationHeader) {
-
-        if(!jwtProvider.isUserAuthorized(userId, authorizationHeader)) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
-
-        User user = userRepository.findById(userId).get();
-
-        if(user.getUserProperties() == null) {
-            user.setUserProperties(userProperties);
-            userPropertiesRepository.save(userProperties);
-            return new ResponseEntity<>(HttpStatus.CREATED);
-        }
-
-        userPropertiesRepository.delete(user.getUserProperties());
-        user.setUserProperties(userProperties);
-        userPropertiesRepository.save(userProperties);
-
-        return new ResponseEntity<>(HttpStatus.OK);
+        return userService.updateUserProperties(userId, userProperties, authorizationHeader);
     }
 
     @PutMapping("/users/{userId}/statistics")
     @Transactional
     public ResponseEntity<?> updateUserStatistics(@PathVariable long userId, @RequestBody UserStatistics userStatistics,
                                                   @RequestHeader(value = "Authorization") String authorizationHeader) {
-
-        if(!jwtProvider.isUserAuthorized(userId, authorizationHeader)) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
-
-        User user = userRepository.findById(userId).get();
-
-        if(user.getUserStatistics() == null) {
-            user.setUserStatistics(userStatistics);
-            userStatisticsRepository.save(userStatistics);
-            return new ResponseEntity<>(HttpStatus.CREATED);
-        }
-
-        userStatisticsRepository.delete(user.getUserStatistics());
-        user.setUserStatistics(userStatistics);
-        userStatisticsRepository.save(userStatistics);
-
-        return new ResponseEntity<>(HttpStatus.OK);
+        return userService.updateUserStatistics(userId, userStatistics, authorizationHeader);
     }
 }
---
+++ src/main/java/com/jakubowski/spring/done/services/UserService.java
@@ -0,0 +1,100 @@
+package com.jakubowski.spring.done.services;
+
+import com.jakubowski.spring.done.entities.User;
+import com.jakubowski.spring.done.entities.UserProperties;
+import com.jakubowski.spring.done.entities.UserStatistics;
+import com.jakubowski.spring.done.repositories.UserPropertiesRepository;
+import com.jakubowski.spring.done.repositories.UserRepository;
+import com.jakubowski.spring.done.repositories.UserStatisticsRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserPropertiesRepository userPropertiesRepository;
+
+    @Autowired
+    private UserStatisticsRepository userStatisticsRepository;
+
+    public User getUserById(long userId, String authorizationHeader) {
+        if(!authService.isUserAuthorized(userId, authorizationHeader)) return null;
+        return userRepository.findById(userId).get();
+    }
+
+    public UserProperties getUserProperties(long userId, String authorizationHeader) {
+        if(!authService.isUserAuthorized(userId, authorizationHeader)) return null;
+        return userRepository.findById(userId).get().getUserProperties();
+    }
+
+    public UserStatistics getUserStatistics(long userId, String authorizationHeader) {
+        if(!authService.isUserAuthorized(userId, authorizationHeader)) return null;
+        return userRepository.findById(userId).get().getUserStatistics();
+    }
+
+    public ResponseEntity<?> deleteUser(long userId, String authorizationHeader) {
+
+        if(!authService.isUserAuthorized(userId, authorizationHeader)) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        userRepository.deleteById(userId);
+        logger.info("User with ID '{}' has been deleted.", userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    public ResponseEntity<?> updateUserProperties(long userId, UserProperties userProperties, String authorizationHeader) {
+
+        if(!authService.isUserAuthorized(userId, authorizationHeader)) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        User user = userRepository.findById(userId).get();
+
+        if(user.getUserProperties() == null) {
+            user.setUserProperties(userProperties);
+            userPropertiesRepository.save(userProperties);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+
+        userPropertiesRepository.delete(user.getUserProperties());
+        user.setUserProperties(userProperties);
+        userPropertiesRepository.save(userProperties);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> updateUserStatistics(long userId, UserStatistics userStatistics, String authorizationHeader) {
+
+        if(!authService.isUserAuthorized(userId, authorizationHeader)) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        User user = userRepository.findById(userId).get();
+
+        if(user.getUserStatistics() == null) {
+            user.setUserStatistics(userStatistics);
+            userStatisticsRepository.save(userStatistics);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+
+        userStatisticsRepository.delete(user.getUserStatistics());
+        user.setUserStatistics(userStatistics);
+        userStatisticsRepository.save(userStatistics);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
