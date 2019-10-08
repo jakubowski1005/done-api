7622b847781fd1b2139507a7cd5cddd4ff09e632
jakubowski1005 <jakubowski1005@gmail.com>
2019-10-08 23:16:21 +0200

    Fix bug with authorization

--- src/main/java/com/jakubowski/spring/done/security/UserSecurity.java
+++ src/main/java/com/jakubowski/spring/done/security/UserSecurity.java
@@ -3,14 +3,10 @@ package com.jakubowski.spring.done.security;
 import com.jakubowski.spring.done.entities.User;
 import com.jakubowski.spring.done.exceptions.ResourcesNotFoundException;
 import com.jakubowski.spring.done.repositories.UserRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 public class UserSecurity {
 
@@ -24,10 +20,7 @@ public class UserSecurity {
                 () -> new ResourcesNotFoundException("User", "ID", userId)
         );
 
-        log.debug("Authentication: ' {} ', credentials: ' {} ', principal: ' {} ', details: ' {} ' ", authentication.toString(), authentication.getCredentials().toString(),
-                authentication.getPrincipal().toString(), authentication.getDetails());
-
-        return true;
-        //return userRepository.findById(userId).isPresent();
+        String current = authentication.getName();
+        return current.equals(user.getUsername());
     }
 }
