10aa76d81641fa8237954dd5d7c94792b1f6fb67
jakubowski1005 <jakubowski1005@gmail.com>
2019-10-08 21:21:02 +0200

    Add log

--- src/main/java/com/jakubowski/spring/done/security/UserSecurity.java
+++ src/main/java/com/jakubowski/spring/done/security/UserSecurity.java
@@ -3,13 +3,17 @@ package com.jakubowski.spring.done.security;
 import com.jakubowski.spring.done.entities.User;
 import com.jakubowski.spring.done.exceptions.ResourcesNotFoundException;
 import com.jakubowski.spring.done.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class UserSecurity {
+
 
     @Autowired
     UserRepository userRepository;
@@ -20,10 +24,10 @@ public class UserSecurity {
                 () -> new ResourcesNotFoundException("User", "ID", userId)
         );
 
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        String principalUsername = userDetails.getUsername();
+        log.debug("Authentication: ' {} ', credentials: ' {} ', principal: ' {} ', details: ' {} ' ", authentication.toString(), authentication.getCredentials().toString(),
+                authentication.getPrincipal().toString(), authentication.getDetails());
 
-        return user.getUsername().equals(principalUsername);
+        return true;
         //return userRepository.findById(userId).isPresent();
     }
 }
