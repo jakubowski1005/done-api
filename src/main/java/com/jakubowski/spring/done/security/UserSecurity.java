3a55acab92198cced2067f0f50c5af8c851e8037
jakubowski1005 <jakubowski1005@gmail.com>
2019-10-08 20:03:53 +0200

    Still tryin

--- src/main/java/com/jakubowski/spring/done/security/UserSecurity.java
+++ src/main/java/com/jakubowski/spring/done/security/UserSecurity.java
@@ -1,25 +1,27 @@
 package com.jakubowski.spring.done.security;
 
+import com.jakubowski.spring.done.entities.User;
+import com.jakubowski.spring.done.exceptions.ResourcesNotFoundException;
+import com.jakubowski.spring.done.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserSecurity {
 
-    public boolean isUserAuthorized(Authentication authentication, Long userId) {
-        System.out.println("Authentication");
-        System.out.println(authentication.toString());
-        System.out.println("getPrincipal()");
-        System.out.println(authentication.getPrincipal().toString());
-        System.out.println("getCredentials()");
-        System.out.println(authentication.getCredentials().toString());
-        System.out.println("getDetails()");
-        System.out.println(authentication.getDetails().toString());
-        System.out.println("getAuthorities");
-        System.out.println(authentication.getAuthorities().toString());
+    @Autowired
+    UserRepository userRepository;
 
-        //JwtUserPrincipal principal = (JwtUserPrincipal) authentication.getPrincipal();
-        //return principal.getId().equals(userId);
-        return true;
+    public boolean isUserAuthorized(Authentication authentication, Long userId) {
+
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new ResourcesNotFoundException("User", "ID", userId)
+        );
+
+        String principalUsername = JwtUserPrincipal.create(user).getUsername();
+        String userUsername = (String) authentication.getPrincipal();
+
+        return userUsername.equals(principalUsername);
     }
 }
