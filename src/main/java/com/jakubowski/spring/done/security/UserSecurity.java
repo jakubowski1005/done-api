545c46b206ea92bddc2fef691019dab8c37de783
jakubowski1005 <jakubowski1005@gmail.com>
2019-10-08 19:38:56 +0200

    Try repair security bug

--- src/main/java/com/jakubowski/spring/done/security/UserSecurity.java
+++ src/main/java/com/jakubowski/spring/done/security/UserSecurity.java
@@ -7,6 +7,17 @@ import org.springframework.stereotype.Component;
 public class UserSecurity {
 
     public boolean isUserAuthorized(Authentication authentication, Long userId) {
+        System.out.println("Authentication");
+        System.out.println(authentication.toString());
+        System.out.println("getPrincipal()");
+        System.out.println(authentication.getPrincipal().toString());
+        System.out.println("getCredentials()");
+        System.out.println(authentication.getCredentials().toString());
+        System.out.println("getDetails()");
+        System.out.println(authentication.getDetails().toString());
+        System.out.println("getAuthorities");
+        System.out.println(authentication.getAuthorities().toString());
+
         //JwtUserPrincipal principal = (JwtUserPrincipal) authentication.getPrincipal();
         //return principal.getId().equals(userId);
         return true;
