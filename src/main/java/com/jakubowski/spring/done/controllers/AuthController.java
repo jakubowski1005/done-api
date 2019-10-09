2dfb59c8c76ef3affee66746b9cebd18bd6feb85
jakubowski1005 <jakubowski1005@gmail.com>
2019-10-09 23:50:55 +0200

    Add cross origin

--- src/main/java/com/jakubowski/spring/done/controllers/AuthController.java
+++ src/main/java/com/jakubowski/spring/done/controllers/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
